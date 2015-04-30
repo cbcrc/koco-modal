4210c71f662783425222c49c2344a226aa3c7a09
fcharest <frederik.charest@radio-canada.ca>
2015-04-30 13:50:51 -0400

    initial commit

---
+++ src/koco-modal.js
@@ -0,0 +1,12 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout-utilities'],
+    function(koUtilities) {
+        'use strict';
+
+        koUtilities.registerComponent('modal', {
+            htmlOnly: true,
+            basePath: 'bower_components/koco-modal/src'
+        });
+    });
